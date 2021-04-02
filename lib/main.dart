335660fca16364903597bb2ae9e0b8e372922f3d
lyrisli <lyrisli@tencent.com>
2021-04-02 14:15:35 +0800

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rx_demo/BlocProvider.dart';
 import 'package:rx_demo/counter/Counter.dart';
-import 'package:rx_demo/counter/CounterBloc.dart';
 import 'package:rx_demo/messages/Message.dart';
 
 void main() {
